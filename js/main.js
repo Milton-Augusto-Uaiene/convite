2bbf34a7bbbd46f98d8ce6fe07f02fa68dec21e7
Milton Augusto Uaieneo <Miltonaugusto137@gmail.com>
2023-03-16 16:25:01 -0700

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,129 @@
+(function ($) {
+    "use strict";
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        loop: true,
+        nav: true,
+        dots: false,
+        items: 1,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    
+})(jQuery);
+
+
+// Relogio
+
+let dayBox = document.getElementById("day-box");
+let hrBox = document.getElementById("hr-box");
+let minBox = document.getElementById("min-box");
+let secBox = document.getElementById("sec-box");
+let endDate = new Date(2024, -9, 1, 00, 00);
+let endTime = endDate.getTime();
+
+function countdown() {
+    let todayDate = new Date();
+    let todayTime = todayDate.getTime();
+    let remainingTime = endTime - todayTime;
+    let oneMin = 60 * 1000;
+    let oneHr = 60 * oneMin;
+    let oneDay = 24 * oneHr;
+
+    let addZeroes = (num) => (num < 10 ? `0${num}` : num)
+
+    if (endTime < todayTime) {
+        clearInterval(i);
+        document.querySelector(
+            ".countdown"
+            ).innerHTML = `<h1>Contagem Terminada</h1>`;
+    } else {
+        let daysLeft = Math.floor(remainingTime / oneDay);
+        let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+        let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+        let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
+
+        dayBox.textContent = addZeroes(daysLeft);
+        hrBox.textContent = addZeroes(hrsLeft);
+        minBox.textContent = addZeroes(minsLeft);
+        secBox.textContent = addZeroes(secsLeft);
+    }
+}
+
+let i = setInterval(countdown, 1000);
+
+countdown();
+
+
+
+
+
+ lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true,
+      'disableScrolling': true
+    })
+
+
+
+
+
+
+
+
+
